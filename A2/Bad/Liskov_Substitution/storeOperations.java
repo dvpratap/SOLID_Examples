b026a18f0a96d07796459a479fa4e5ea42052530
Dev Pratap Singh Rajawat <dv269119@dal.ca>
2022-10-30 23:01:21 -0300

    added liskov bad example

--- A2/Bad/Liskov_Substitution/storeOperations.java
+++ A2/Bad/Liskov_Substitution/storeOperations.java
@@ -1,13 +1,18 @@
 public abstract class storeOperations {
+    protected String storeType = "Food";
     protected float itemPrice;
     protected String itemName;
     protected int quantity;
     protected String customerName;
     protected String email;
     public float generateBill() {
-        getProductPrice(itemName);
-        float bill = (itemPrice * quantity);
-        return ((bill * 0.2f)+ bill);
+        float totalbill=0.00f;
+        if(storeType.equals("Food")) {
+            getProductPrice(itemName);
+            float bill = (itemPrice * quantity);
+            totalbill = ((bill * 0.2f) + bill);
+        }
+        return totalbill;
     }
     public void getCustomerDetails(String name, String mail){
         customerName = name;
